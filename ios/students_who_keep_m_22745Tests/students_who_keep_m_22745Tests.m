55bb1fac670d2dca04dc9e350767a2cfe0e84459
Crowdbotics <team@crowdbotics.com>
2020-11-19 00:36:06 +0000

    Initial commit.

---
+++ ios/students_who_keep_m_22745Tests/students_who_keep_m_22745Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface students_who_keep_m_22745 : XCTestCase
+
+@end
+
+@implementation students_who_keep_m_22745
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
